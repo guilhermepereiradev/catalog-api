ce59f5da781a71131357c9f340bfd54951ebf884
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2024-03-01 22:59:39 -0300

    trigger ci

--- src/test/java/br/com/grupo5/catalog/CatalogApplicationTests.java
+++ src/test/java/br/com/grupo5/catalog/CatalogApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.grupo5.catalog;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class CatalogApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(6 < 1);
 	}
 
 }
